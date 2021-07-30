e1ddd4af44d31d7d34813fcf9ab977c28a2bb6f9
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-07-30 15:31:39 +0200

    aggiunto es email

--- es-mail/js/script.js
+++ es-mail/js/script.js
@@ -3,7 +3,13 @@ var listamail = ["roccorocco@gmail.com", "pippopippo@gmail.com"];
 //Chiedi all’utente la sua email,
 var email = prompt("inserisci la tua mail");
 
-listamail.push (email)
+listamail.push (email);
 
 //controlla che sia nella lista di chi può accedere,
 //stampa un messaggio appropriato sull’esito del controllo.
+
+for ( var i = 0; i < listamail.length; i++) {
+    if ( listamail [i] == email) {
+        alert("Benvenuto!")
+    }
+}
