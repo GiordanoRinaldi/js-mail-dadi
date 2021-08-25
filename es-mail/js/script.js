78fd672c773cc9122a107d9f59dfbc4aedec0dd0
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-08-25 17:31:05 +0200

    fix es email 2.0

--- es-mail/js/script.js
+++ es-mail/js/script.js
@@ -3,8 +3,6 @@ var listamail = ["roccorocco@gmail.com", "pippopippo@gmail.com"];
 //Chiedi all’utente la sua email,
 var email = prompt("inserisci la tua mail");
 
-listamail.push (email);
-
 //controlla che sia nella lista di chi può accedere,
 //stampa un messaggio appropriato sull’esito del controllo.
 
